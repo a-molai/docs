5d6e4e70be1d13e222c0af56749f2a5a169659f2
A. Mola'i <a-molai@github.com>
2018-11-04 18:21:21 +0330

    working on the project

---
+++ files/styles/login.js
@@ -0,0 +1,23 @@
+function check_mail() {
+  if ($("#login-form input:text:first").val() == "پست الکترونیک") 
+    $("#login-form input:text:first").val('');
+  $("#login-form input:text:first").css({
+    "font-family": "SegoeUI-Regular",
+    "font-size": "11pt",
+    "direction": "ltr",
+    "color": "#000"
+  });
+}
+
+function check_password() {
+    if ($("#login-form input:text:nth-child(2)").val() == "رمز عبور") 
+      $("#login-form input:text:nth-child(2)").val('');
+    $("#login-form input:text:nth-child(2)").attr('type', 'password');
+    $("#login-form input:nth-child(2)").css({
+      "border": "none",
+      "font-family": "SegoeUI-Regular",
+      "font-size": "11pt",
+      "direction": "ltr",
+      "color": "#000"
+    });
+  }
---
+++ login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>ورود به سامانه</title>
+    <link rel="stylesheet" href="Files/Style/Reset.css"/>
+    <link rel="stylesheet" href="Files/Style/Master.css"/>
+    <link rel="stylesheet" href="Files/Style/Fonts.css"/>
+    <link rel="stylesheet" href="Files/Style/Login.css"/>
+    <script src="Files/Style/JQuery-v3.3.1.js"></script>
+    <script src="Files/Style/Login.js"></script>
+  </head>
+  <body>
+    <div id="login-form">
+      <img src="Files/Images/Logo.png"/>
+      <div>
+        <input type="text" value="پست الکترونیک" onfocus="check_mail()"/>
+        <input type="text" value="رمز عبور" onfocus="check_password()"/>
+        <input type="button" value="ورود به سامانه"/>
+      </div>
+      <p>Copyright &copy; 2018 Amin IDC. All Rights Reserved.</p>
+    </div>
+  </body>
+</html>
