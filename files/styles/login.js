edb1c2afa5040cc9848ae2c4d6f2c9e976246ea8
Amir Hosein <ah.mollaei@amin.dc>
2018-11-06 23:07:36 +0330

    working on the project

--- files/styles/login.js
+++ files/styles/login.js
@@ -5,7 +5,7 @@ $(function() {
       $("#login-form input:text:first").val("");
       $("#login-form input:text:first").css({
         "font-family": "SegoeUI-Regular",
-        "font-size": "16px",
+        "font-size": "15px",
         "padding": "",
         "color": "#000"
       });
