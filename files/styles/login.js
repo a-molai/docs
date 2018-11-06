3cad5d0f459ffa693411b0cf37be029c44e704cb
Amir Hosein <ah.mollaei@amin.dc>
2018-11-06 22:46:37 +0330

    working on the project

--- files/styles/login.js
+++ files/styles/login.js
@@ -6,7 +6,6 @@ $(function() {
       $("#login-form input:text:first").css({
         "font-family": "SegoeUI-Regular",
         "font-size": "11pt",
-        "direction": "ltr",
         "color": "#000"
       });
     }
@@ -17,8 +16,7 @@ $(function() {
       $("#login-form input:text:first").val("نام کاربری");
       $("#login-form input:text:first").css({
         "font-family": "iyekan-regular",
-        "font-size": "11.5pt",
-        "direction": "ltr",
+        "font-size": "11pt",
         "color": "#999"
       });
     }
