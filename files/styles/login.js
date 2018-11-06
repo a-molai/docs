975f5bb88897aa874caf89ff4a32524551325af6
Amir Hosein <ah.mollaei@amin.dc>
2018-11-06 23:15:33 +0330

    working on the project

--- files/styles/login.js
+++ files/styles/login.js
@@ -5,7 +5,7 @@ $(function() {
       $("#login-form input:text:first").val("");
       $("#login-form input:text:first").css({
         "font-family": "SegoeUI-Regular",
-        "padding": "9px 10%",
+        "padding": "10px 10%",
         "color": "#000"
       });
     }
@@ -17,7 +17,7 @@ $(function() {
       $("#login-form input:text:first").css({
         "font-family": "iyekan-regular",
         "padding": "10px 10%",
-        "color": "#f00"
+        "color": "#0f0"
       });
     }
   });
