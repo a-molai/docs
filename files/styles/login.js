171382e9abd822f5c021e373192893587793bb61
Amir Hosein <ah.mollaei@amin.dc>
2018-11-06 23:41:59 +0330

    working on the project

--- files/styles/login.js
+++ files/styles/login.js
@@ -28,6 +28,8 @@ $(function() {
       $("#login-form input:text:nth-child(2)").val("");
       $("#login-form input:text:nth-child(2)").attr({
         "type": "password",
+      });
+      $("#login-form input:text:nth-child(2)").css({
         "width": "60%",
         "padding": "10px 10%",
         "margin": "0px 10% 10px 10%",
@@ -37,10 +39,6 @@ $(function() {
         "background-color": "#ddd",
         "border": "none",
         "color": "#f00"
-      });
-      $("#login-form input:text:nth-child(2)").css({
-        "padding-bottom": "11px",
-        "color": "#000"
       });
     }
   });
