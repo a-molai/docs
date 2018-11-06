8b6be669b31515e72258446a942c3bd42c36fd2c
Amir Hosein <ah.mollaei@amin.dc>
2018-11-06 21:24:03 +0330

    working on the project

---
+++ login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>ورود به سامانه</title>
+    <link rel="stylesheet" href="files/styles/reset.css"/>
+    <link rel="stylesheet" href="files/styles/master.css"/>
+    <link rel="stylesheet" href="files/styles/fonts.css"/>
+    <link rel="stylesheet" href="files/styles/login.css"/>
+    <script src="files/styles/jquery-v3.3.1.js"></script>
+    <script src="files/styles/login.js"></script>
+  </head>
+  <body>
+    <div id="login-form">
+      <img src="files/images/logo.png"/>
+      <div>
+        <input type="text" value="پست الکترونیک" onfocus="check_mail()"/>
+        <input type="text" value="رمز عبور" onfocus="check_password()"/>
+        <a href="dashboard.html"><input type="button" value="ورود به سامانه"/></a>
+      </div>
+      <!--<p>Copyright &copy; 2018 Amin IDC. All Rights Reserved.</p>-->
+    </div>
+  </body>
+</html>
