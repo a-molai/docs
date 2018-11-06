fad661866d18a26562de72e90da4208541682f9d
Amir Hosein <ah.mollaei@amin.dc>
2018-11-06 22:20:00 +0330

    working on the project

--- files/styles/login.js
+++ files/styles/login.js
@@ -1,11 +1,12 @@
 $(function() {
-  $("#login-form input:text:first").click(function() {
+  $("#login-form input:text:first").focusin(function() {
     if ($("#login-form input:text:first").val() == "پست الکترونیک")
       $("#login-form input:text:first").val("");
   });
   $("#login-form input:text:first").focusout(function() {
     if ($("#login-form input:text:first").val() == "")
       $("#login-form input:text:first").val("پست الکترونیک");
+  });
 });
 
 
