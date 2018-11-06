94070f4a5d56dcea18694803dfbc8d93c28992de
Amir Hosein <ah.mollaei@amin.dc>
2018-11-06 22:31:13 +0330

    working on the project

--- files/styles/login.js
+++ files/styles/login.js
@@ -2,6 +2,12 @@ $(function() {
   $("#login-form input:text:first").focusin(function() {
     if ($("#login-form input:text:first").val() == "نام کاربری")
       $("#login-form input:text:first").val("");
+      $("#login-form input:text:first").css({
+        "font-family": "SegoeUI-Regular",
+        "font-size": "11pt",
+        "direction": "ltr",
+        "color": "#000"
+      });
   });
   $("#login-form input:text:first").focusout(function() {
     if ($("#login-form input:text:first").val() == "")
@@ -16,61 +22,3 @@ $(function() {
       $("#login-form input:text:nth-child(2)").val("رمز عبور");
   });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function username() {
-  if ($("#login-form input:text:first").val() == "نام کاربری")
-    $("#login-form input:text:first").val('');
-  $("#login-form input:text:first").css({
-    "font-family": "SegoeUI-Regular",
-    "font-size": "11pt",
-    "direction": "ltr",
-    "color": "#000"
-  });
-}
-
-function password() {
-    if ($("#login-form input:text:nth-child(2)").val() == "رمز عبور") 
-      $("#login-form input:text:nth-child(2)").val('');
-    $("#login-form input:text:nth-child(2)").attr('type', 'password');
-    $("#login-form input:nth-child(2)").css({
-      "border": "none",
-      "font-family": "SegoeUI-Regular",
-      "font-size": "11pt",
-      "direction": "ltr",
-      "color": "#000"
-    });
-  }
