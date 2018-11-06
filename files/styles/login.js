1e3dc4f2db3d51668a56818f578273001461dac8
Amir Hosein <ah.mollaei@amin.dc>
2018-11-06 22:43:19 +0330

    working on the project

--- files/styles/login.js
+++ files/styles/login.js
@@ -8,7 +8,8 @@ $(function() {
         "font-size": "11pt",
         "direction": "ltr",
         "color": "#000"
-      })};
+      });
+    }
   });
 
   $("#login-form input:text:first").focusout(function() {
@@ -19,29 +20,8 @@ $(function() {
         "font-size": "11.5pt",
         "direction": "ltr",
         "color": "#999"
-      })};
+      });
+    }
   });
 
-  
-  $("#login-form input:text:nth-child(2)").focusin(function() {
-    if ($("#login-form input:text:nth-child(2)").val() == "رمز عبور")
-      $("#login-form input:text:nth-child(2)").val("");
-      $("#login-form input:text:nth-child(2)").css({
-        "font-family": "SegoeUI-Regular",
-        "font-size": "11pt",
-        "direction": "ltr",
-        "color": "#000"
-      });
-  });
-
-  $("#login-form input:text:nth-child(2)").focusout(function() {
-    if ($("#login-form input:text:nth-child(2)").val() == "")
-      $("#login-form input:text:nth-child(2)").val("رمز عبور");
-      $("#login-form input:text:nth-child(2)").css({
-        "font-family": "iyekan-regular",
-        "font-size": "12pt",
-        "direction": "ltr",
-        "color": "#999"
-      });
-  });
 });
