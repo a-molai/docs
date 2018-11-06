078c54b31acb64b0babd827edac588b7e10b2fcc
Amir Hosein <ah.mollaei@amin.dc>
2018-11-06 22:40:12 +0330

    working on the project

--- files/styles/login.js
+++ files/styles/login.js
@@ -1,25 +1,25 @@
 $(function() {
 
   $("#login-form input:text:first").focusin(function() {
-    if ($("#login-form input:text:first").val() == "نام کاربری")
+    if ($("#login-form input:text:first").val() == "نام کاربری") {
       $("#login-form input:text:first").val("");
       $("#login-form input:text:first").css({
         "font-family": "SegoeUI-Regular",
         "font-size": "11pt",
         "direction": "ltr",
         "color": "#000"
-      });
+      })};
   });
 
   $("#login-form input:text:first").focusout(function() {
-    if ($("#login-form input:text:first").val() == "")
+    if ($("#login-form input:text:first").val() == "") {
       $("#login-form input:text:first").val("نام کاربری");
       $("#login-form input:text:first").css({
         "font-family": "iyekan-regular",
-        "font-size": "11pt",
+        "font-size": "11.5pt",
         "direction": "ltr",
         "color": "#999"
-      });
+      })};
   });
 
   
