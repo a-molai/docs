61068e3936bb0df7b8e02faa7fc015a0505f2289
Amir Hosein <ah.mollaei@amin.dc>
2018-11-07 17:59:56 +0330

    working on the project

--- files/styles/login.js
+++ files/styles/login.js
@@ -35,12 +35,11 @@ $(function() {
 
   $("#login-form input:text:nth-child(2)").focusout(function() {
     if ($("#login-form input:text:nth-child(2)").val() == "") {
-      $("#login-form input:text:nth-child(2)").val("رمز عبور");
-      $("#login-form input:text:nth-child(2)").css({
-        "font-family": "iyekan-regular",
-        "padding-bottom": "10px",
-        "color": "#999"
+      $("#login-form input:text:nth-child(2)").attr({
+        "type": "text",
+        "style": "width: 60%; padding: 11px 10% 9px 10%; margin: 0px 10% 10px 10%; font-family: 'iyekan-regular'; font-size: 15px; text-align: center; background-color: #ddd; border: none; color: #000"
       });
+      $("#login-form input:text:nth-child(2)").val("رمز عبور");
     }
   });
 
