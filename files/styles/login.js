ff53d03a5a19d3a682dd6de885fa20aa03c9d824
Amir Hosein <ah.mollaei@amin.dc>
2018-11-07 20:29:34 +0330

    working on the project

--- files/styles/login.js
+++ files/styles/login.js
@@ -29,7 +29,7 @@ $(function() {
       $(this).val("");
       $(this).attr({
         "type": "password",
-        "style": "width: 90%; margin: 11px 5% 0px 5%; background-color: transparent; font-family: 'iyekan-regular'; font-size: 15px; text-align: center; border: none; color: 222;"
+        "style": "width: 90%; margin: 10px 5% 0px 5%; background-color: transparent; font-family: 'iyekan-regular'; font-size: 15px; text-align: center; border: none; color: 222;"
       });
     };
   });
