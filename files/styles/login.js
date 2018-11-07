3327261e8fc358e3e645f635fec69fc9e5b79176
Amir Hosein <ah.mollaei@amin.dc>
2018-11-07 21:59:02 +0330

    working on the project

--- files/styles/login.js
+++ files/styles/login.js
@@ -29,7 +29,7 @@ $(function() {
       $(this).val("");
       $(this).attr({
         "type": "password",
-        "style": "width: 90%; margin: 9px 5% 0px 5%; background-color: transparent; font-family: 'segoeui-regular'; font-size: 15px; text-align: center; border: none; color: 222; box-sizing: border-box"
+        "style": "width: 90%; margin: 9px 5% 0px 5%; background-color: transparent; font-family: 'segoeui-regular'; font-size: 15px; text-align: center; border: none; color: #222;"
       });
     };
   });
@@ -39,7 +39,7 @@ $(function() {
       $(this).val("رمز عبور");
       $(this).attr({
         "type": "text",
-        "style": "width: 90%; margin: 9px 5% 0px 5%; background-color: transparent; font-family: 'iyekan-regular'; font-size: 15px; text-align: center; border: none; color: 222; box-sizing: border-box"
+        "style": "width: 90%; margin: 8px 5% 0px 5%; background-color: transparent; font-family: 'iyekan-regular'; font-size: 15px; text-align: center; border: none; color: #222;"
       });
     };
   });
