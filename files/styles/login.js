90f15bebf6d0e856cf27515be5f246388ea1cdf6
Amir Hosein <ah.mollaei@amin.dc>
2018-11-07 19:46:03 +0330

    working on the project

--- files/styles/login.js
+++ files/styles/login.js
@@ -1,9 +1,9 @@
 $(function() {
 
   $("input:text:first").focusin(function() {
-    if ($("input:text:first").val() == "نام کاربری") {
-      $("input:text:first").val("");
-      $("input:text:first").css({
+    if ($(this).val() == "نام کاربری") {
+      $(this).val("");
+      $(this).css({
         "font-family": "segoeui-regular",
         "font-size": "16px",
         "margin-top": "6px",
@@ -13,9 +13,9 @@ $(function() {
   });
 
   $("input:text:first").focusout(function() {
-    if ($("input:text:first").val() == "") {
-      $("input:text:first").val("نام کاربری");
-      $("input:text:first").css({
+    if ($(this).val() == "") {
+      $(this).val("نام کاربری");
+      $(this).css({
         "font-family": "iyekan-regular",
         "font-size": "15px",
         "margin-top": "8px",
@@ -24,28 +24,7 @@ $(function() {
     };
   });
 
-  $("input:text:nth-child(2)").focusin(function() {
-    if ($("input:text:nth-child(2)").val() == "p") {
-      $("input:text:nth-child(2)").val("");
-      $("input:text:nth-child(2)").css({
-        "font-family": "segoeui-regular",
-        "font-size": "16px",
-        "margin-top": "6px",
-        "color": "#222"
-      });
-    };
-  });
-
-  $("input:text:nth-child(2)").focusout(function() {
-    if ($("input:text:nth-child(2)").val() == "") {
-      $("input:text:nth-child(2)").val("p");
-      $("input:text:nth-child(2)").css({
-        "font-family": "iyekan-regular",
-        "font-size": "15px",
-        "margin-top": "8px",
-        "color": "#888"
-      });
-    };
-  });
+  $("input[type=text]:nth-child").focusin(function() {
+    $(this)
 
 });
