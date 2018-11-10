af81c777c993b8b76f7e9ea9d2bfb48933ac0945
Amir Hosein <ah.mollaei@amin.dc>
2018-11-10 17:21:16 +0330

    working on the project

--- tmp.php
+++ tmp.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>DOCS (Home)</title>
+    <title>~tmp!</title>
     <link rel="stylesheet" href="files/styles/reset.css"/>
     <link rel="stylesheet" href="files/styles/master.css"/>
     <link rel="stylesheet" href="files/styles/fonts.css"/>
