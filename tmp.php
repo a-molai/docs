d27b1c353aa3c679bb6cf593cf4d031df27baf46
Amir Hosein <ah.mollaei@amin.dc>
2018-11-10 16:49:55 +0330

    working on the project

--- tmp.php
+++ tmp.php
@@ -6,14 +6,16 @@
     <link rel="stylesheet" href="files/styles/reset.css"/>
     <link rel="stylesheet" href="files/styles/master.css"/>
     <link rel="stylesheet" href="files/styles/fonts.css"/>
-    <link rel="stylesheet" href="files/styles/home.css"/>
+    <link rel="stylesheet" href="files/styles/tmp.css"/>
     <script src="files/styles/jquery-v3.3.1.js"></script>
     <script src="files/styles/login.js"></script>
   </head>
   <body>
     <div>
-</div>
+
+    </div>
     <div>
-</div>
+    </div>
+
   </body>
 </html>
