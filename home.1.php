76eefcb72cf8e615f1c1cbecb280c4344bd74af2
Amir Hosein <ah.mollaei@amin.dc>
2018-11-10 16:48:46 +0330

    working on the project

---
+++ home.1.php
@@ -0,0 +1,54 @@
+﻿<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>DOCS (Home)</title>
+    <link rel="stylesheet" href="files/styles/reset.css"/>
+    <link rel="stylesheet" href="files/styles/master.css"/>
+    <link rel="stylesheet" href="files/styles/fonts.css"/>
+    <link rel="stylesheet" href="files/styles/home.css"/>
+    <script src="files/styles/jquery-v3.3.1.js"></script>
+    <script src="files/styles/login.js"></script>
+  </head>
+  <body>
+    <div>
+      <img src="files/images/logo.png"/>
+      <ul>
+          <a href="#"><li>آرشیو گزارشات</li></a>
+          <a href="login.php"><li>خروج</li></a>
+      </ul>
+    </div>
+    <ul id="events">
+      <li>
+        <span>یکشنبه مورخ 15 شهریور 97</span>
+        <span> | </span>
+        <span>Thursday 17 November 2018</span>
+      </li>
+      <li>
+          <p>12:40</p>
+          <p>به نام خدا</p>
+          <p>جناب آقای ایروانی</p>
+      </li>
+      <li>
+          <p>12:40</p>
+          <p>به نام خدا</p>
+          <p>جناب آقای ایروانی</p>
+      </li>
+      <li>
+          <p>12:40</p>
+          <p>به نام خدا</p>
+          <p>جناب آقای ایروانی</p>
+      </li>
+      <li>
+          <p>12:40</p>
+          <p>به نام خدا</p>
+          <p>جناب آقای ایروانی</p>
+      </li>
+      <li>
+          <p>12:40</p>
+          <p>به نام خدا</p>
+          <p>جناب آقای ایروانی</p>
+      </li>
+    </ul>
+  </body>
+</html>
