413a0bf1d2474125018ec6cb2f5b39aa76ff00e3
Amir Hosein <ah.mollaei@amin.dc>
2018-11-10 10:34:23 +0330

    working on the project

--- home.php
+++ home.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>DOCS [Home]</title>
+    <title>DOCS (Home)</title>
     <link rel="stylesheet" href="files/styles/reset.css"/>
     <link rel="stylesheet" href="files/styles/master.css"/>
     <link rel="stylesheet" href="files/styles/fonts.css"/>
@@ -15,7 +15,7 @@
       <img src="files/images/logo.png"/>
       <ul>
         <a href=""><li>آرشیو گزارشات</li></a>
-        <a href=""><li>خروج</li></a>
+        <a href="login.php"><li>خروج</li></a>
       </ul>
     </div>
     <div>
